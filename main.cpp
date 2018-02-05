e3fd426fb50543b8620f08d4ebd957effa8d534f
DirtbagDrew <andrewmyerhb@gmail.com>
2018-02-05 17:51:13 -0500

    menu finished

--- main.cpp
+++ main.cpp
@@ -16,6 +16,9 @@ int inputIntRange(int low, int high)
 void menu()
 {
   int menuInput = 0;
+  int pointerToAccess = 0;
+  int subMenuInput=0;
+
   while(menuInput!=4)
   {
     cout<<"(1) Access A Pointer"<<endl;
@@ -26,15 +29,41 @@ void menu()
 
     switch(menuInput)
     {
-      case 1:
-        
+      case 1: //access a pointer
+        cout<<"Which Pointer Do You Want to Access?: ";
+        pointerToAccess=inputIntRange(0,19);
+        cout<<"You Selected pointer: "<<pointerToAccess<<". What Would You Like to Do?"<<endl;
+        cout<<"(1) Print the First 10 Characters in the Array"<<endl;
+        cout<<"(2) Delete All Chars Associated with Pointer"<<endl;
+        cout<<"(3) Return to Main Menu"<<endl;
+        subMenuInput = inputIntRange(1,3);
+        switch(subMenuInput)
+        {
+          case 1: //print first 10 characters in array
+            cout<<"First 10 Characters:"<<endl;
+            break;
+          case 2: //delete all characters associated with pointer
+            cout<<"Deleting All Characters Associated with "<<pointerToAccess<<endl;
+            break;
+          case 3: //return to main menu
+            cout<<"Returning to Main Menu"<<endl;
+            cout<<endl;
+            break;
+        }
         break;
-      case 2:
+
+      case 2: //list deallocated memory
+        cout<<"Deallocated Memory: "<<endl;
 
         break;
-      case 3:
+      case 3: //deallocate all memory
+        cout<<"Deallocating All Memory"<<endl;
 
-        break
+        break;
+      case 4: //exit program
+        cout<<"Exiting Program";
+
+        break;
     }
   }
 }
